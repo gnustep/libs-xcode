930e715e221497b707de87a75491a2fe75e2f3a9
Gregory John Casamento <greg.casamento@gmail.com>
2011-11-05 21:04:33 +0000

    added setenv implementation to allow this to work on windows.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/xcode/trunk@34125 72102866-910b-0410-8b05-ffd578937521

---
+++ setenv.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <strings.h>
+
+int setenv(const char *key, const char *value, int overwrite)
+{
+  int result = 255; // failure...
+  char *current = getenv(key);
+  if(overwrite && current)
+    {
+      int key_len = strlen(key);
+      int val_len = sttlen(value);
+      int len = key_len + 1 + val_len + 1; // key + "=" + value + '\0'
+      char *buffer = malloc(len);
+      bzero(buffer, len);
+      sprintf(buffer,"%s=%s",key,value);
+      result = putenv(buffer);
+      free(buffer);
+    }
+  return result;
+}
