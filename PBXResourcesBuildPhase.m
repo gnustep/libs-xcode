cfb70c51c78ae9cdc8a775cb42c73c06f5fd8b95
Gregory John Casamento <greg.casamento@gmail.com>
2012-02-25 16:36:47 +0000

    * PBXResourcesBuildPhase.m: Fix issues with copying of resources 	which are localized.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/xcode/trunk@34818 72102866-910b-0410-8b05-ffd578937521

--- PBXResourcesBuildPhase.m
+++ PBXResourcesBuildPhase.m
@@ -29,10 +29,27 @@
       NSString *fileName = [filePath lastPathComponent];
       NSString *destPath = [resourcesDir stringByAppendingPathComponent: fileName];
       NSError *error = nil;
+      BOOL copyResult = NO; 
+
+      // If there is more than one path component... then the intervening directories need to
+      // be created.
+      if([[filePath pathComponents] count] > 1)
+	{
+	  NSString *dirs = [filePath stringByDeletingLastPathComponent];
+
+	  destPath = [resourcesDir stringByAppendingPathComponent: dirs];
+	  [[NSFileManager defaultManager] createDirectoryAtPath:destPath
+				    withIntermediateDirectories:YES
+						     attributes:nil
+							  error:NULL];
+	  destPath = [destPath stringByAppendingPathComponent: fileName];
+	}
+      
       NSLog(@"\tCopy %@ -> %@",filePath,destPath);
-      BOOL copyResult = [[NSFileManager defaultManager] copyItemAtPath: filePath
-								toPath: destPath
-								 error: &error];
+      copyResult = [[NSFileManager defaultManager] copyItemAtPath: filePath
+							   toPath: destPath
+							    error: &error];
+
       if(!copyResult)
 	{
 	  NSLog(@"\tFile exists...");
@@ -51,9 +68,9 @@
   NSString *awkCommand = [NSString stringWithFormat: 
 				     @"awk '{while(match($0,\"[$]{[^}]*}\")) {var=substr($0,RSTART+2,RLENGTH -3);gsub(\"[$]{\"var\"}\",ENVIRON[var])}}1' < %@ > %@",
 				   [inputPlist stringByEscapingSpecialCharacters], [outputPlist stringByEscapingSpecialCharacters]];
-  GSXCBuildContext *context = [GSXCBuildContext sharedBuildContext];
-  NSString *modified = [context objectForKey: @"MODIFIED_FLAG"];
   int sysresult = 0;
+  // GSXCBuildContext *context = [GSXCBuildContext sharedBuildContext];
+  // NSString *modified = [context objectForKey: @"MODIFIED_FLAG"];
   // if([modified isEqualToString: @"YES"])
     {
       NSLog(@"\t%@",awkCommand);
