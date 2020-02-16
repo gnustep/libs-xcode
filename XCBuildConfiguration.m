845e8e77db639c047bbb6c0238af6e420a3991c5
Gregory John Casamento <greg.casamento@gmail.com>
2020-02-16 14:02:39 -0500

    Add debug info

--- XCBuildConfiguration.m
+++ XCBuildConfiguration.m
@@ -34,10 +34,10 @@
   NSEnumerator *en = [buildSettings keyEnumerator];
   NSString *key = nil;
 
-  while((key = [en nextObject]) != nil)
+  while ((key = [en nextObject]) != nil)
     {
       id value = [buildSettings objectForKey: key];
-      if([value isKindOfClass: [NSString class]])
+      if ([value isKindOfClass: [NSString class]])
 	{	  
 	  NSDebugLog(@"\tEnvironment: %@ = %@",key,value);
 	  setenv([key cString],[value cString],1);
@@ -46,21 +46,22 @@
 	{
 	  [context setObject: value
 		      forKey: key];
-	  NSDebugLog(@"\t    Context: %@ = %@",key,value);
+	  NSDebugLog(@"\tContext: %@ = %@",key,value);
 	}
       else
 	{
 	  NSDebugLog(@"\tWARNING: Can't interpret value %@, for environment variable %@", value, key); 
 	}
     }
-  if([buildSettings objectForKey: @"TARGET_BUILD_DIR"] == nil)
+  
+  if ([buildSettings objectForKey: @"TARGET_BUILD_DIR"] == nil)
     {
-      //      puts("\tEnvironment: TARGET_BUILD_DIR = build (built-in)");
+      NSDebugLog(@"\tEnvironment: TARGET_BUILD_DIR = build (built-in)");
       setenv("TARGET_BUILD_DIR","build",1);
     }
-  if([buildSettings objectForKey: @"BUILT_PRODUCTS_DIR"] == nil)
+  if ([buildSettings objectForKey: @"BUILT_PRODUCTS_DIR"] == nil)
     {
-      // puts("\tEnvironment: BUILT_PRODUCTS_DIR = build (built-in)");
+      NSDebugLog(@"\tEnvironment: BUILT_PRODUCTS_DIR = build (built-in)");
       setenv("BUILT_PRODUCTS_DIR","build",1);
     }
   puts([[NSString stringWithFormat: @"=== Done Applying Build Configuration for %@",name] cString]);
