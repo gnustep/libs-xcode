8ce52a09b474cb5e3c6507b47deea6413944369e
Gregory John Casamento <greg.casamento@gmail.com>
2022-04-13 20:51:41 -0400

    Update so that build method returns NO

--- GSXCGenerator.h
+++ GSXCGenerator.h
@@ -1,7 +1,16 @@
 #import <Foundation/NSObject.h>
 #import "GSXCCommon.h"
 
+@class PBXNativeTarget;
+
 @interface GSXCGenerator : NSObject
+{
+  PBXNativeTarget *_target;
+}
+
+- (instancetype) initWithTarget: (PBXNativeTarget *)target;
+
+- (PBXNativeTarget *) target;
 
 - (BOOL) build;
 
--- GSXCGenerator.m
+++ GSXCGenerator.m
@@ -3,12 +3,28 @@
 
 #import "GSXCGenerator.h"
 #import "GSXCCommon.h"
+#import "PBXNativeTarget.h"
 
 @implementation GSXCGenerator
 
+- (instancetype) initWithTarget: (PBXNativeTarget *)target
+{
+  self = [super init];
+  if (self != nil)
+    {
+      _target = target;
+    }
+  return self;
+}
+
+- (PBXNativeTarget *) target
+{
+  return _target;
+}
+
 - (BOOL) build
 {
-  return [self notImplemented: _cmd];
+  return ([self notImplemented: _cmd] != nil);
 }
 
 @end
