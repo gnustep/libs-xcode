d02c437a13acfe44e9972cfc872fc5a40297e5d5
Gregory John Casamento <greg.casamento@gmail.com>
2022-04-29 08:22:31 -0400

    Add code to create sections properly

--- Generators/VisualStudio/GSXCVSGlobalSection.m
+++ Generators/VisualStudio/GSXCVSGlobalSection.m
@@ -5,9 +5,78 @@
 
 @implementation GSXCVSGlobalSection
 
+- (id) objectForKey: (NSString *)k
+{
+  return [_values objectForKey: k];
+}
+    
+- (void) setObject: (id)o forKey: (NSString *)k
+{
+  [_values setObject: o
+              forKey: k];
+}
+
+- (GSXCVSGlobalSectionType) type
+{
+  return _type;
+}
+
+- (void) setType: (GSXCVSGlobalSectionType)type
+{
+  _type = type;
+}
+
+- (NSString *) stringForType
+{
+  NSString *result = nil;
+  
+  switch (_type)
+    {
+    case SolutionConfigurationPlatforms:
+      {
+        result = @"SolutionConfigurationPlatforms";
+        break;
+      }
+    case ProjectConfigurationPlatforms:
+      {
+        result = @"ProjectConfigurationPlatforms";
+        break;
+      }
+    case ProjectSolutionProperties: 
+      {
+        result = @"ProjectSolutionProperties";
+        break;
+      }
+    case ExtensibilityGlobals:
+      {
+        result = @"ExtensibilityGoals";
+        break;
+      }
+    default:
+      {
+        result = nil;
+        break;
+      }
+    }
+
+  return result;
+}
+
 - (NSString *) string
 {
-  return @"";
+  NSString *result = [NSString stringWithFormat: @"\tGlobalSection(%@)\n", [self stringForType]];
+  NSEnumerator *en = [_values keyEnumerator];
+  NSString *k = nil;
+
+  while ((k = [en nextObject]) != nil)
+    {
+      id v = [_values objectForKey: k];
+      result = [result stringByAppendingString: [NSString stringWithFormat: @"\t\t%@ = %@\n", k, v]];
+    }
+
+  result = [result stringByAppendingString: @"\tEndGlobalSection"];
+  
+  return result;
 }
 
 @end
