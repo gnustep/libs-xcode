5b3fb67ac09e889ed7e69d76328974a4ec3dc4e5
Gregory John Casamento <greg.casamento@gmail.com>
2022-11-23 04:56:19 -0500

    Revert xcsystem...

--- XCode/xcsystem.m
+++ XCode/xcsystem.m
@@ -23,7 +23,6 @@
 */
 
 #import <Foundation/NSUUID.h>
-#import <Foundation/NSTask.h>
 
 #import "GSXCBuildContext.h"
 #import "NSString+PBXAdditions.h"
@@ -33,8 +32,6 @@
 #ifndef _MSC_VER
 #import <unistd.h>
 #endif
-
-extern char **environ;
 
 NSString *contextString()
 {
@@ -78,88 +75,55 @@ NSInteger xcsystem(NSString *cmd)
   NSDictionary *dict = [ctx config];
   NSString *setupScript = [dict objectForKey: @"setupScript"];
   NSInteger r = 0;
-  NSString *scriptFormat = @"#!/bin/bash\n"
-    @"set -eo pipefail\n"
-    @"shopt -s inherit_errexit\n\n"
-    @"%@\n\n"
-    @"exit $?\n";
-  NSString *body = @"";
-  NSString *script = nil;
-  NSString *c = [cmd stringByReplacingOccurrencesOfString: @"$(SRCROOT)" withString: @"."];
 
-  if (setupScript != nil)
+  if(setupScript == nil)
     {
-      body = [body stringByAppendingString: [NSString stringWithFormat: @". %@ > /dev/null\n", setupScript]];
-    }
-  
-  body = [body stringByAppendingString: contextString()];
-  body = [body stringByAppendingString: [NSString stringWithFormat: @"%@\n", c]];
-  script = [NSString stringWithFormat: scriptFormat, body];
-  
-  NSString *filename = [NSString stringWithFormat: @"bt_%lu.sh", [script hash]];
-  NSString *scriptCmd = [NSString stringWithFormat: @"build/%@", filename];
-  BOOL f = [script writeToFile: scriptCmd
-		    atomically: YES
-		      encoding: NSUTF8StringEncoding
-			 error: NULL];
-
-  // make script executable
-  if (f)
-    {
-      NSFileManager *mgr = [NSFileManager defaultManager];
-      NSString *octal = @"0755"; // permissions
-      unsigned long value = 0;
-
-      f = NO;
-      sscanf([octal UTF8String], "%lo", &value);
-
-      if (value != 0)
-	{
-	  NSNumber *n = [NSNumber numberWithLong: value];
-	  NSDictionary *attrs = [NSDictionary dictionaryWithObjectsAndKeys: n, NSFilePosixPermissions, nil];
-	  
-	  f = [mgr changeFileAttributes: attrs
-				 atPath: scriptCmd];
-	}
-      else
-	{
-	  NSLog(@"Couldn't scan permissions value");
-	}
-    }
-  else
-    {
-      NSLog(@"Unable to write %@", scriptCmd);
-    }
-
-  // run it...
-  if (f)
-    {
-      NSTask *task;
-      
 #ifdef _WIN32
-      NSString *win_cmd = [scriptCmd execPathForString];
-
+      NSString *win_cmd = [cmd execPathForString];
+      
       NSDebugLog(@"win_cmd = %@", win_cmd);
-      task = [NSTask launchedTaskWithLaunchPath: [win_cmd stringByResolvingPath]
-				      arguments: nil];
+      r = system([win_cmd cString]);
 #else
-      task = [NSTask launchedTaskWithLaunchPath: [scriptCmd stringByResolvingPath]
-				      arguments: nil];
+      r = system([cmd cString]);
 #endif
-      if (task)
-	{
-	  [task waitUntilExit];
-	}
-      else
-	{
-	  NSLog(@"Failed to start task %@", scriptCmd);
-	}
     }
   else
     {
-      NSLog(@"Failed to permissions for script %@", scriptCmd);
-    }
+      NSString *scriptFormat = @"#!/bin/bash\n"
+	@"set -eo pipefail\n"
+	@"shopt -s inherit_errexit\n\n"
+	@"%@\n\n"
+	@"exit $?\n";
+      NSString *body = @"";
+      NSString *script = nil;
+      NSString *c = [cmd stringByReplacingOccurrencesOfString: @"$(SRCROOT)" withString: @"."];
+      body = [body stringByAppendingString: [NSString stringWithFormat: @". %@ > /dev/null\n", setupScript]];
+      body = [body stringByAppendingString: contextString()];
+      body = [body stringByAppendingString: [NSString stringWithFormat: @"%@\n", c]];
 
+      script = [NSString stringWithFormat: scriptFormat, body];
+
+      NSString *filename = [NSString stringWithFormat: @"bt_%lu.sh", [script hash]];
+      NSString *scriptCmd = [NSString stringWithFormat: @"./build/%@", filename];
+      BOOL f = [script writeToFile: scriptCmd
+			atomically: YES
+			  encoding: NSUTF8StringEncoding
+			     error: NULL];
+      if (f)
+	{
+#ifdef _WIN32
+	  NSString *win_cmd = [scriptCmd execPathForString];
+	  NSDebugLog(@"win_cmd = %@", win_cmd);
+	  r = system([win_cmd cString]);
+#else
+	  r = system([scriptCmd cString]);
+#endif
+	}
+      else
+	{
+	  NSLog(@"Failed to write out temporary script %@", scriptCmd);
+	}
+    }
   return r;
 }
 
