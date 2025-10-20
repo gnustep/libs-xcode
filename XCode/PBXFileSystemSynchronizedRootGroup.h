91ef10c4e8b34046a1a42f04cabaa7dd9cc7c6bb
Gregory John Casamento <greg.casamento@gmail.com>
2025-10-20 17:15:46 -0400

    fix file system sync

--- XCode/PBXFileSystemSynchronizedRootGroup.h
+++ XCode/PBXFileSystemSynchronizedRootGroup.h
@@ -3,5 +3,41 @@
 // Local includes
 #import "PBXGroup.h"
 
+@class PBXNativeTarget;
+
 @interface PBXFileSystemSynchronizedRootGroup : PBXGroup
+{
+  PBXNativeTarget *_target;
+}
+
+/**
+ * Returns the synchronized children for this group by scanning the file system.
+ */
+- (NSMutableArray *) synchronizedChildren;
+
+/**
+ * Refreshes the synchronized children by rescanning the file system.
+ */
+- (void) refreshChildren;
+
+/**
+ * Sets the target for this group.
+ */
+- (void) setTarget: (PBXNativeTarget *)target;
+
+/**
+ * Gets the target for this group.
+ */
+- (PBXNativeTarget *) target;
+
+/**
+ * Builds the files in this synchronized group.
+ */
+- (BOOL) build;
+
+/**
+ * Generates output for the files in this synchronized group.
+ */
+- (BOOL) generate;
+
 @end
--- XCode/PBXFileSystemSynchronizedRootGroup.m
+++ XCode/PBXFileSystemSynchronizedRootGroup.m
@@ -26,22 +26,44 @@
 #import "PBXFileSystemSynchronizedRootGroup.h"
 #import "PBXFileReference.h"
 #import "PBXBuildFile.h"
+#import "PBXNativeTarget.h"
 #import "NSString+PBXAdditions.h"
 
 @implementation PBXFileSystemSynchronizedRootGroup
 
+- (void) dealloc
+{
+  // _target is not retained, so just clear the reference
+  _target = nil;
+  
+  [super dealloc];
+}
+
 - (NSMutableArray *) synchronizedChildren
 {
   NSFileManager *fm = [NSFileManager defaultManager];
-  NSDirectoryEnumerator *den = [fm enumeratorAtPath: _path];
+  NSString *groupPath = [self path];
   NSMutableArray *result = [NSMutableArray array];
+  
+  if (groupPath == nil)
+    {
+      return result;
+    }
+    
+  NSDirectoryEnumerator *den = [fm enumeratorAtPath: groupPath];
   NSString *filePath = nil;
   NSMutableArray *fp = [NSMutableArray array];
+  
+  if (den == nil)
+    {
+      NSLog(@"Warning: Could not enumerate path '%@' for PBXFileSystemSynchronizedRootGroup", groupPath);
+      return result;
+    }
   
   while ((filePath = [den nextObject]) != nil)
     {
       BOOL isDir = NO;
-      NSString *fullPath = [_path stringByAppendingPathComponent: filePath];
+      NSString *fullPath = [groupPath stringByAppendingPathComponent: filePath];
       
       if ([fm fileExistsAtPath: fullPath isDirectory: &isDir] && isDir == NO)
 	{
@@ -78,7 +100,65 @@
 - (void) setPath: (NSString *)path
 {
   [super setPath: path];
+  [self refreshChildren];
+}
+
+- (void) refreshChildren
+{
   [self setChildren: [self synchronizedChildren]];
+}
+
+- (void) setTarget: (PBXNativeTarget *)target
+{
+  // Don't use ASSIGN here since this might create a retain cycle
+  _target = target;
+}
+
+- (PBXNativeTarget *) target
+{
+  return _target;
+}
+
+- (BOOL) build
+{
+  NSArray *children = [self children];
+  NSEnumerator *en = [children objectEnumerator];
+  PBXBuildFile *buildFile = nil;
+  BOOL result = YES;
+  
+  while ((buildFile = [en nextObject]) != nil && result)
+    {
+      if (_target != nil)
+        {
+          [buildFile setTarget: _target];
+        }
+      result = [buildFile build];
+    }
+  
+  return result;
+}
+
+- (BOOL) generate
+{
+  NSArray *children = [self children];
+  NSEnumerator *en = [children objectEnumerator];
+  PBXBuildFile *buildFile = nil;
+  BOOL result = YES;
+  
+  xcputs([[NSString stringWithFormat: @"=== Processing PBXFileSystemSynchronizedRootGroup: %@", [self path]] cString]);
+  
+  while ((buildFile = [en nextObject]) != nil && result)
+    {
+      if (_target != nil)
+        {
+          [buildFile setTarget: _target];
+        }
+      result = [buildFile generate];
+    }
+  
+  xcputs([[NSString stringWithFormat: @"=== Completed PBXFileSystemSynchronizedRootGroup: %@", [self path]] cString]);
+  
+  return result;
 }
 
 @end
